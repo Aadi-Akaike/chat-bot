670aabb26aeb1c629b6a63e6f7f0b9c663652bee
Aadi-Akaike <adithya@akaiketech.com>
2024-04-03 19:07:34 +0530

    initial commit

---
+++ components/Chat/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="bg-blue-200 rounded-lg w-[400px] h-[600px] flex flex-col px-6 py-4">
+      <p className="text-lg font-semibold text-black">Chat bot</p>
+    </div>
+  );
+};
+
+export default Chat;
